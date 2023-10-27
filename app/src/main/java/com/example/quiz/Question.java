ac26b3495b4b63edbfc9b3a1bfbb03645f327b1a
MRodriguezValdes <mrodriguezvaldes@cifpfbmoll.eu>
2023-10-27 19:29:47 +0200

    Work it

---
+++ app/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,26 @@
+package com.example.quiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
